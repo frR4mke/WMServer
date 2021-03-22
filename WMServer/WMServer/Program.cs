de1f4569db8a29f1188d5c0c10117f8c460875dd
frR4mke <44607143+frR4mke@users.noreply.github.com>
2021-03-22 15:06:15 +0300

    no message

--- WMServer/WMServer/Program.cs
+++ WMServer/WMServer/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
 namespace WMServer
 {
@@ -13,13 +15,17 @@ namespace WMServer
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			CreateWebHostBuilder(args).Build().Run();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder => {
-					webBuilder.UseStartup<Startup>();
+					webBuilder.UseStartup<Startup>().UseUrls("http://localhost:50001");
 				});
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.UseUrls("http://localhost:50001");
 	}
 }
