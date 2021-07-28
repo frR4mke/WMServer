d26456873b253a7c39084a0bb0a486523140811e
frR4mke <44607143+frR4mke@users.noreply.github.com>
2021-07-28 18:52:41 +0300

    коммит

--- WMServer/WMBLogic/Embedded/DTO/DTOProducts.sql
+++ WMServer/WMBLogic/Embedded/DTO/DTOProducts.sql
@@ -8,7 +8,7 @@ from Products p
          left join Images i on p.product_id = i.product_id
          left join PercentageDiscount pd on p.product_id = pd.product_id
          left join GiftGoods gg on p.product_id = gg.product_id
-         left join Products gp on gg.giftproduct_id = gp.product_idфывфбвлф
+         left join Products gp on gg.giftproduct_id = gp.product_id
 -- group by p.product_id, p.product_title, p.quantity,
 --          p.manufacturer_id, p.description, p.full_product_title,
 --          p.sort_order, p.productType_id, p.country,p.subProductType_id,
